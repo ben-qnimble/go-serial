7b69b0cd64f621ed51fab262a8591fbaae015184
Ben Luey <57039667+ben-qnimble@users.noreply.github.com>
2023-11-21 17:41:31 -0500

    Add testing for interface number on windows

--- enumerator/usb_windows_test.go
+++ enumerator/usb_windows_test.go
@@ -20,24 +20,27 @@ func parseAndReturnDeviceID(deviceID string) *PortDetails {
 
 func TestParseDeviceID(t *testing.T) {
 	r := require.New(t)
-	test := func(deviceId, vid, pid, serialNo string) {
+	test := func(deviceId, vid, pid, serialNo, mi string) {
 		res := parseAndReturnDeviceID(deviceId)
 		r.True(res.IsUSB)
 		r.Equal(vid, res.VID)
 		r.Equal(pid, res.PID)
 		r.Equal(serialNo, res.SerialNumber)
+                if mi != "" {
+                    r.Equal(mi,res.MI)
+                }
 	}
 
-	test("FTDIBUS\\VID_0403+PID_6001+A6004CCFA\\0000", "0403", "6001", "A6004CCFA")
-	test("USB\\VID_16C0&PID_0483\\12345", "16C0", "0483", "12345")
-	test("USB\\VID_2341&PID_0000\\64936333936351400000", "2341", "0000", "64936333936351400000")
-	test("USB\\VID_2341&PID_0000\\6493234373835191F1F1", "2341", "0000", "6493234373835191F1F1")
-	test("USB\\VID_2341&PID_804E&MI_00\\6&279A3900&0&0000", "2341", "804E", "")
-	test("USB\\VID_2341&PID_004E\\5&C3DC240&0&1", "2341", "004E", "")
-	test("USB\\VID_03EB&PID_2111&MI_01\\6&21F3553F&0&0001", "03EB", "2111", "") // Atmel EDBG
-	test("USB\\VID_2341&PID_804D&MI_00\\6&1026E213&0&0000", "2341", "804D", "")
-	test("USB\\VID_2341&PID_004D\\5&C3DC240&0&1", "2341", "004D", "")
-	test("USB\\VID_067B&PID_2303\\6&2C4CB384&0&3", "067B", "2303", "") // PL2303
+	test("FTDIBUS\\VID_0403+PID_6001+A6004CCFA\\0000", "0403", "6001", "A6004CCFA","")
+	test("USB\\VID_16C0&PID_0483\\12345", "16C0", "0483", "12345","")
+	test("USB\\VID_2341&PID_0000\\64936333936351400000", "2341", "0000", "64936333936351400000","")
+	test("USB\\VID_2341&PID_0000\\6493234373835191F1F1", "2341", "0000", "6493234373835191F1F1","")
+	test("USB\\VID_2341&PID_804E&MI_00\\6&279A3900&0&0000", "2341", "804E", "","00")
+	test("USB\\VID_2341&PID_004E\\5&C3DC240&0&1", "2341", "004E", "","")
+	test("USB\\VID_03EB&PID_2111&MI_01\\6&21F3553F&0&0001", "03EB", "2111", "","01") // Atmel EDBG
+	test("USB\\VID_2341&PID_804D&MI_00\\6&1026E213&0&0000", "2341", "804D", "","00")
+	test("USB\\VID_2341&PID_004D\\5&C3DC240&0&1", "2341", "004D", "","")
+	test("USB\\VID_067B&PID_2303\\6&2C4CB384&0&3", "067B", "2303", "","") // PL2303
 }
 
 func TestParseDeviceIDWithInvalidStrings(t *testing.T) {
